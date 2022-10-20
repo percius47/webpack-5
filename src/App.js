abc09c700b67b8a87191f3892944769b2bb16ce2
percius47 <33738242+percius47@users.noreply.github.com>
2022-10-20 22:54:57 +0530

    devServer error

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import _ from 'lodash'
+import "./App.css";
+const App = ({ name}) => {
+	return `
+    <h2> Welcome, ${name} </h2>
+    `;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import App from "./App";
+// import Logo
+import Logo from './logo.svg'
+
+// inside innerHTML
 
 const root = document.getElementById("root");
 root.innerHTML = `
 <h1> learning web tooling today </h1>
+<img src='${Logo}' alt='build icon' />
 ${App({ name: "Tanay" })}
 `;
